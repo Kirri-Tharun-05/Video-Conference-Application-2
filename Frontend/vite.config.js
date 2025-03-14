470c33895ce5099a4bcbd1955c32b325eace6d75
Kirri Tharun <tharunprajitha2023@gmail.com>
2025-03-14 20:50:08 +0530

    Updated

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -5,22 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-  tailwindcss()],
-  build: {
-    outDir: 'dist', // Ensure output is in the "dist" folder
-  },
-  server: {
-    port: 3000, // You can change this if needed
-    strictPort: true,
-  },
-  preview: {
-    port: 4173, // Ensure the preview runs correctly
-    strictPort: true,
-  },
-  base: "/", // Ensures correct base path
-  resolve: {
-    alias: {
-      '@': '/src',
-    },
-  },
+  tailwindcss()]
 })
